d0374ad1323870f58caf27254fffe0ce6893ff7c
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2022-04-29 12:56:19 +0530

    First Commit

---
+++ api/Backend/Backend/Controllers/DepartmentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select DepartmentID, DepartmentName from dbo.Department";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeApplicationConnection");
+            SqlDataReader myReader;
+            using(SqlConnection myConnection= new SqlConnection(sqlDataSource))
+            {
+                myConnection.Open();
+                using(SqlCommand myCommand = new SqlCommand(query, myConnection))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConnection.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
